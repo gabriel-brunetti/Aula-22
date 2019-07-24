17aa54b9d7823df3f5b3f7ae0031149015a57fa6
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2019-07-24 10:39:06 -0300

    adicionando inputs e botões

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <input type="text" name="nome" id="nome" placeholder="Digite seu nome">
+    <input type="email" name="email" id="email" placeholder="Digite seu e-mail">
+    <button type="submit">Salvar</button>
+</body>
+</html>
